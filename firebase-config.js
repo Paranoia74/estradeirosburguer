56c008334176862fde36fa002f79fc4009e57819
Paranoia74 <vitor.rodriguestaddeo@hotmail.com>
2025-10-31 16:36:27 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDq3B1K1V5tE90b5Enr___2NQ9e2wIJOY",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDq38IlKl5VTE9Ob5Enr___2NQ9e2wIJOY",
   authDomain: "estradeiro-burguer.firebaseapp.com",
   projectId: "estradeiro-burguer",
-  storageBucket: "estradeiro-burguer.appspot.com",   // <-- troque para .appspot.com
+  storageBucket: "estradeiro-burguer.firebasestorage.app",
   messagingSenderId: "463453759377",
   appId: "1:463453759377:web:90c0cea2b8267acaf81c30",
   measurementId: "G-S2GES525TF"
