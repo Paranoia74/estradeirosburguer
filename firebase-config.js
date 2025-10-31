74f0da1ae4384b0092c6aca84325149f8d795220
Paranoia74 <vitor.rodriguestaddeo@hotmail.com>
2025-10-31 17:04:43 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDq38IlKl5VTE9Ob5Enr___2NQ9e2wIJOY",
+// Configuração Firebase - Estradeiros Burguer
+export const firebaseConfig = {
+  apiKey: "AIzaSyDqIVznrZms0zmJ0D45l8euiFluyRI7v0I",
   authDomain: "estradeiro-burguer.firebaseapp.com",
   projectId: "estradeiro-burguer",
-  storageBucket: "estradeiro-burguer.firebasestorage.app",
+  storageBucket: "estradeiro-burguer.appspot.com",
   messagingSenderId: "463453759377",
-  appId: "1:463453759377:web:90c0cea2b8267acaf81c30",
-  measurementId: "G-S2GES525TF"
+  appId: "1:463453759377:web:e1556a50e820514ff81c30",
+  measurementId: "G-H5EFQKWCJ7"
 };
+
