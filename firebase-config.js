8e378aab336c93a107075722155f41fec7be60c2
Paranoia74 <vitor.rodriguestaddeo@hotmail.com>
2025-10-31 16:24:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Configuração do Firebase — Estradeiros Burguer
 export const firebaseConfig = {
-  apiKey: "AIzaSyDq38IlKl5VTE9Ob5Enr___2NQ9e2wIJOY",
+  apiKey: "AIzaSyDq3B1K1V5tE90b5Enr___2NQ9e2wIJOY",
   authDomain: "estradeiro-burguer.firebaseapp.com",
   projectId: "estradeiro-burguer",
-  storageBucket: "estradeiro-burguer.firebasestorage.app",
+  storageBucket: "estradeiro-burguer.appspot.com",   // <-- troque para .appspot.com
   messagingSenderId: "463453759377",
   appId: "1:463453759377:web:90c0cea2b8267acaf81c30",
   measurementId: "G-S2GES525TF"
