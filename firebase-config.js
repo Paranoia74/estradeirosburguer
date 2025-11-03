a1a84c958842cc0942b185aaec9c3ec74ce4bd12
Paranoia74 <vitor.rodriguestaddeo@hotmail.com>
2025-11-03 17:32:10 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,14 @@
-// firebase-config.js
-export const firebaseConfig = {
+
+// firebase-config.js — exporta SOMENTE o objeto de configuração
+const firebaseConfig = {
+  // ⚠️ COLE AQUI sua configuração exata do Firebase:
   apiKey: "AIzaSyDqIVznrZms0zmJ0D45l8euiFluyRI7v0I",
   authDomain: "estradeiro-burguer.firebaseapp.com",
-  projectId: "estradeiro-burguer",
-  storageBucket: "estradeiro-burguer.appspot.com",
-  messagingSenderId: "464354375977",
+ projectId: "estradeiro-burguer",
+   storageBucket: "estradeiro-burguer.appspot.com",
+  messagingSenderId: "463453759377",
   appId: "1:464354375977:web:e15565a50e8205144ff61c30",
-  measurementId: "G-H5EFQKWJC7"
-  // no fim do arquivo:
+   measurementId: "G-H5EFQKWJC7"
 };
+
+export default firebaseConfig;
